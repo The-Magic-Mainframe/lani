25941f2dc6e0483a6bfb53db01c9c4547fe63092
spnab <nicholas-a-becker@protonmail.com>
2025-05-17 12:48:18 -0500

    all instructions

--- src/all.py
+++ src/all.py
@@ -5,5 +5,10 @@ all.py - All objects in the `lani` package.
 # core objects
 from .core import *
 
-# general instructions
+# all instructions
+from .control.all import *
+from .decimal.all import *
+from .floating_point.all import *
 from .general.all import *
+from .io.all import *
+from .vector.all import *
